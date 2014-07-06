5e564dc41ca4e03bae84b0c6be568a8fc552829e
jamesyothers <james.yothers@gmail.com>
2014-07-06 16:42:39 -0700

    Firebase setup

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('fastpass.controllers', [])
 
 .controller('DashCtrl', function($scope) {
 })
--- www/js/services.js
+++ www/js/services.js
@@ -1,4 +1,4 @@
-angular.module('starter.services', [])
+angular.module('fastpass.services', [])
 
 /**
  * A simple example service that returns some data.
