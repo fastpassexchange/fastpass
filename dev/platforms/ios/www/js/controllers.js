df4705a04b8aa2c041ab336907155b46344d6cee
jamesyothers <james.yothers@gmail.com>
2014-07-06 18:07:40 -0700

    created prod and dev folders

---
+++ dev/platforms/ios/www/js/controllers.js
@@ -0,0 +1,15 @@
+angular.module('fastpass.controllers', [])
+
+.controller('DashCtrl', function($scope) {
+})
+
+.controller('FriendsCtrl', function($scope, Friends) {
+  $scope.friends = Friends.all();
+})
+
+.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+  $scope.friend = Friends.get($stateParams.friendId);
+})
+
+.controller('AccountCtrl', function($scope) {
+});
