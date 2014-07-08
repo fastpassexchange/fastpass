6d64926b9eaf7c7b8184f8a26fb83fed8dc3d722
jamesyothers <james.yothers@gmail.com>
2014-07-07 19:54:27 -0700

    new offer writing to firebase from form

--- dev/www/js/controllers.js
+++ dev/www/js/controllers.js
@@ -1,18 +1,30 @@
-angular.module('fastpass.controllers', ['ionic'])
+angular.module('fastpass.controllers', ['ionic', 'firebase'])
 
 .controller('listController', function($scope, $state, listService) {
   $scope.text = listService;
   // the following line does not work, why?
   // service.$bind($scope, "text");
-  $scope.addOffer = function() {
-    // add details from form to database
-    // $scope.text.$add({});
-    // $scope.text
-  };
+  // $scope.addOffer = function() {
+  //   // add details from form to database
+  //   // $scope.text.$add({});
+  //   // $scope.text
+  // };
 })
 
-.controller('formController', function($scope, formService) {
-  
+// .controller('listController', ['$scope', 'listService',
+//   function($scope, service) {
+//     service.$bind($scope, 'text');
+//   }
+// ])
+
+.controller('offerController', function($scope, $firebase, formService) {
+  $scope.offer = {};
+  $scope.addOffer = function() {
+    $scope.offer.createdAt = new Date();
+    console.log($scope.offer);
+    var offerRef = new Firebase('https://fastpass-connection.firebaseio.com/offers');
+    $firebase(offerRef).$add($scope.offer);
+  };
 })
 
 .controller('HomeTabCtrl', function($scope) {
