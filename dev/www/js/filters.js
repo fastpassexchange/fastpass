722da8d1c23990a2fff3eb0659bd2906d3aa6995
Alexander Gugel <alexander.gugel@gmail.com>
2014-07-23 10:34:35 -0700

    (refact) passes: Extract logic into filters

--- dev/www/js/filters.js
+++ dev/www/js/filters.js
@@ -1,5 +1,8 @@
 angular.module('fastpass.filters', [])
 
+// Given a user id in the format (facebook|google):[uid], this function returns
+// an URL for the profile picture, which can be used later on to display an
+// avatar.
 .filter('profilePicture', ['$http', function ($http) {
   return function (input) {
     input = input.split(':');
@@ -11,6 +14,8 @@ angular.module('fastpass.filters', [])
   };
 }])
 
+// Given a user id in the format (facebook|google):[uid], this function returns
+// an URL pointing to the social profile of the user (Facebook or Google).
 .filter('profile', [function () {
   return function (input) {
     input = input.split(':');
@@ -19,5 +24,31 @@ angular.module('fastpass.filters', [])
     } else if (input[0] === 'google') {
       return 'https://plus.google.com/u/0/' + input[1] + '/posts';
     }
+  };
+}])
+
+// Filter out offers that are not available.
+.filter('available', [function () {
+  return function (offers) {
+    var available = [];
+    angular.forEach(offers, function (offer) {
+      if (offer.available) {
+        available.push(offer);
+      }
+    });
+    return available;
+  };
+}])
+
+// Excule all the offers made by this specific user.
+.filter('excludeMyOffers', ['authService', function (authService) {
+  return function (offers) {
+    var byOthers = [];
+    angular.forEach(offers, function (offer) {
+      if (authService.getUserId() !== offer.offererId) {
+        byOthers.push(offer);
+      }
+    });
+    return byOthers;
   };
 }]);
