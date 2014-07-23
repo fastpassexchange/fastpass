42edfc13f8bac835ed5c56b622e1f44d42cc60eb
joshWyatt <joshuawyatt123@gmail.com>
2014-07-23 10:42:32 -0700

    Change `starter` to read `fastpass`

--- dev/www/js/app.js
+++ dev/www/js/app.js
@@ -1,10 +1,10 @@
 // Ionic Starter App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// 'fastpass' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+// 'fastpass.controllers' is found in controllers.js
+angular.module('fastpass', ['ionic', 'fastpass.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
