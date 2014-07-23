d20c78283ce46ac9e4a90ef45928b7c59741bb35
joshWyatt <joshuawyatt123@gmail.com>
2014-07-23 10:54:31 -0700

    Inject firebase into app.js

--- dev/www/js/app.js
+++ dev/www/js/app.js
@@ -1,10 +1,4 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'fastpass' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'fastpass.controllers' is found in controllers.js
-angular.module('fastpass', ['ionic', 'fastpass.controllers'])
+angular.module('fastpass', ['ionic', 'fastpass.controllers', 'firebase'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
