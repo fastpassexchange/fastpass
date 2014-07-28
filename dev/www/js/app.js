80fffa8659fd67cc93b4d7740428cb777a75f481
jamesyothers <james.yothers@gmail.com>
2014-07-28 13:23:43 -0700

    accessory bar added to keyboard plugin

--- dev/www/js/app.js
+++ dev/www/js/app.js
@@ -5,7 +5,7 @@ angular.module('fastpass', ['ionic', 'fastpass.controllers', 'firebase', 'fastpa
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
     }
     if(window.StatusBar) {
       // org.apache.cordova.statusbar required
